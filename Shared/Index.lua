7d70380eed3e877bdfa5175e78d47c1a42b1c8e2
Mouloud <mouloudhooden@gmail.com>
2022-09-08 10:07:07 +0200

    Added Events

--- Shared/Index.lua
+++ Shared/Index.lua
@@ -7,4 +7,7 @@ print("Start Shared Package ...")
 Package.Require("utils/sh_util.lua")
 Package.Require("utils/sh_command.lua")
 
+Package.Require("modules/jobs/sh_core.lua")
+Package.Require("modules/money/sh_core.lua")
+
 print("End Shared Package ...")
