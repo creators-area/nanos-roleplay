ca52eba31de164d9a1624ddbaf9d556ff7b4e8ab
Mouloud <mouloudhooden@gmail.com>
2022-09-08 15:08:26 +0200

    add setMoney and setHunger

--- Server/modules/money/sv_core.lua
+++ Server/modules/money/sv_core.lua
@@ -10,3 +10,14 @@ function Player:addMoney(money, callback)
         callback(newMoney)
     end, newMoney, self:GetSteamID())
 end
+
+function Player:setMoney(money, callback)
+    local oldMoney = self:getMoney()
+    local newMoney = money
+    self:SetValue("NanosRP::Money", newMoney)
+    NanosRP:Query("UPDATE `NanosRP_Players` SET `money` = ? WHERE `steamid` = ?", function()
+        Events.Call("NanosRP::ValueUpdate", self, "NanosRP::Money", oldMoney, newMoney)
+        if not callback then return end
+        callback(newMoney)
+    end, newMoney, self:GetSteamID())
+end
