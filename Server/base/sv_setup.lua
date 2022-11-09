3b7f8973079b24d42779e859eaac168157963561
MouloudP <44133428+MouloudP@users.noreply.github.com>
2022-11-09 11:22:54 +0100

    Update sv_setup.lua

--- Server/base/sv_setup.lua
+++ Server/base/sv_setup.lua
@@ -21,3 +21,29 @@ Player.Subscribe("Spawn", function(ply)
         Events.CallRemote("NanosRP::SpawnPoint::Create", ply)
     end
 end)
+
+Events.Subscribe("NanosRP::SpawnPoint::Create", function(ply, pos, rot)
+    if ply:GetSteamID() ~= NanosRP.Owner then
+        return
+    end
+    NanosRP:Insert("INSERT INTO `NanosRP_Spawn` (`posX`, `posY`, `posZ`, `rotX`, `rotY`, `rotZ`) VALUES (?, ?, ?, ?, ?, ?)", pos.x, pos.y, pos.z, rot.x, rot.y, rot.z, function()
+        NanosRP:Select("SELECT * FROM `NanosRP_Spawn`", function(result)
+            if result and result[1] then
+                NanosRP.SpawnPoints = result
+            end
+        end)
+    end)
+end)
+
+Events.Subscribe("NanosRP::SpawnPoint::Remove", function(ply, id)
+    if ply:GetSteamID() ~= NanosRP.Owner then
+        return
+    end
+    NanosRP:Delete("DELETE FROM `NanosRP_Spawn` WHERE `id` = ?", id, function()
+        NanosRP:Select("SELECT * FROM `NanosRP_Spawn`", function(result)
+            if result and result[1] then
+                NanosRP.SpawnPoints = result
+            end
+        end)
+    end)
+end)
